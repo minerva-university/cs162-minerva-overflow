9521f35970b4c71133cd2191157c38c2d7679313
Zhenia-Magic <evgeniia@minerva.kgi.edu>
2021-11-03 17:21:54 +0100

    [Database] Fixed bug with the app and db integration

--- application/app.py
+++ application/app.py
@@ -1,22 +1,24 @@
+from config import Config
 from flask import Flask, render_template, request, redirect, url_for, Response
 from typing import List
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-
 from models import *
-
-"""
-All of these route functions are not tested and given just for the reference of the other backend developers. Consider 
-testing them, or rewriting them if needed.
-"""
+from extensions import db
 
 
-# Home page
+def create_app() -> Flask:
+    application = Flask(__name__)
+    application.config.from_object(Config)
+    application.app_context().push()
+    db.init_app(application)
+    with application.app_context():
+        db.create_all()
+        db.session.commit()
+    return application
+
+
+app = create_app()
+
+
 @app.route("/", methods=['POST'])
 def index():
     return render_template('index.html')
@@ -126,12 +128,6 @@ def get_posts_by_city(city_id: int) -> List[Post]:
 
 def get_posts_by_cohoty(cohort_id: int) -> List[Post]:
     return Cohort.query.filter_by(cohort_id=cohort_id).first().posts
-
-
-def create_app() -> Flask:
-    db.init_app(app)
-    db.create_all()
-    return app
 
 
 if __name__ == '__main__':
