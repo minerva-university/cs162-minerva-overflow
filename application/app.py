d59a46fdb343bb909ef7df3e2cec29fcad0b1818
Nazar <yaremko.nazar2000@gmail.com>
2021-11-04 15:25:37 +0100

    Added skeleton from login and registration forms, and loging, register, logout methods

--- application/app.py
+++ application/app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for, Response
 from typing import List
 from models import *
 from extensions import db
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+from application.forms import LoginForm, RegistrationForm
 
 
 def create_app() -> Flask:
@@ -129,6 +131,58 @@ def get_posts_by_city(city_id: int) -> List[Post]:
 def get_posts_by_cohoty(cohort_id: int) -> List[Post]:
     return Cohort.query.filter_by(cohort_id=cohort_id).first().posts
 
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    #if the user is alredy aithenitificated then take them back to the home page
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    #get data form the registration form
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        #hash the password for security reasons
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        #create an instance of a new user with their data in the database
+        #NEEDS TO BE CHANGED
+        #user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        #flash the message that the account has been created
+        flash('Your account has been created! You are now able to log in', 'success')
+        #and redirect the user to the login page
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        #if the user is alredy aithenitificated then take them back to the home page
+        return redirect(url_for('home'))
+    #get the data from the login form
+    form = LoginForm()
+    if form.validate_on_submit():
+        #get information from the database that corresponds to data submited in the login form
+        #NEEDS TO BE CHANGED
+        user = User.query.filter_by(email=form.email.data).first()
+        #if the user exists and the password is right
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            #then we log the user in
+            login_user(user, remember=form.remember.data)
+            #and take them to the next page
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
+        #if some data is incorrect, the message is flashed
+        if not user or not bcrypt.check_password_hash(user.password, form.password.data):
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+
+#logs the user out
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app = create_app()
