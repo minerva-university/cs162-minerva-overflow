2118e3bb50cb24858029929b81d8b0720122c116
yutaro-shimizu <68834841+yutaro-shimizu@users.noreply.github.com>
2021-11-04 21:00:45 +0900

    fixed errors in App.js and components/Header.js

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Header contact={{username: XYZ}}/>
+        <Header contact={{username: "XYZ"}}/>
         
       </header>
     </div>
--- react-flask-app/src/components/Header.js
+++ react-flask-app/src/components/Header.js
@@ -1,3 +1,4 @@
+import logo from '../logo.svg';
 import React from "react"
 import ReactDom from "react-dom"
 
