6c704c9152d05cbc12d80df37588883c02851afc
sherlockieee <phuonghatrannguyen00@gmail.com>
2021-11-17 10:24:54 +0100

    add initialized data for user and post

--- application/models.py
+++ application/models.py
@@ -337,6 +337,53 @@ def insert_initial_tags(target: Table, connection: Connection, **kw):
         connection.execute(target.insert(), {"tag_name": tag})
 
 
+def insert_initial_users(target: Table, connection: Connection, **kw):
+    user1 = User(
+        user_name="ZheniaMagic",
+        user_password="Minerva",
+        first_name="Evgeniia",
+        surname="Buzulukova",
+        email="evgeniia@uni.minerva.edu",
+        cohort_id=5,
+        about_me="I love coding",
+    )
+
+    user2 = User(
+        user_name="sherlockieee",
+        user_password="123",
+        first_name="Ha",
+        surname="Tran",
+        email="tnp_ha@uni.minerva.edu",
+        cohort_id=5,
+        about_me="I love boba",
+    )
+
+    db.session.add(user1)
+    db.session.add(user2)
+    db.session.commit()
+
+
+def insert_initial_posts(target: Table, connection: Connection, **kw):
+
+    post1 = Post(
+        user_id=1,
+        city_id=2,
+        title="Best Donuts in USA",
+        post_text="Bob's Donuts are so so great. I love them!",
+    )
+    post2 = Post(
+        user_id=1,
+        city_id=2,
+        title="AWS Loft",
+        post_text="Nice coworking space available for free",
+    )
+    db.session.add(post1)
+    db.session.add(post2)
+    db.session.commit()
+
+
 event.listen(Tag.__table__, "after_create", insert_initial_tags)
 event.listen(City.__table__, "after_create", insert_initial_cities)
 event.listen(Cohort.__table__, "after_create", insert_initial_cohorts)
+event.listen(User.__table__, "after_create", insert_initial_users)
+event.listen(Post.__table__, "after_create", insert_initial_posts)
--- conftest.py
+++ conftest.py
@@ -17,43 +17,6 @@ def client() -> Flask:
     with app.app_context():
         db.create_all()
 
-        user1 = User(
-            user_name="ZheniaMagic",
-            user_password="Minerva",
-            first_name="Evgeniia",
-            surname="Buzulukova",
-            email="evgeniia@uni.minerva.edu",
-            cohort_id=5,
-            about_me="I love coding",
-        )
-
-        user2 = User(
-            user_name="sherlockieee",
-            user_password="123",
-            first_name="Ha",
-            surname="Tran",
-            email="tnp_ha@uni.minerva.edu",
-            cohort_id=5,
-            about_me="I love boba",
-        )
-        post1 = Post(
-            user_id=1,
-            city_id=2,
-            title="Best Donuts in USA",
-            post_text="Bob's Donuts are so so great. I love them!",
-        )
-        post2 = Post(
-            user_id=1,
-            city_id=2,
-            title="AWS Loft",
-            post_text="Nice coworking space available for free",
-        )
-        db.session.add(user1)
-        db.session.add(user2)
-        db.session.add(post1)
-        db.session.add(post2)
-
-        db.session.commit()
     yield client
     with app.app_context():
         db.drop_all()
