01b418d6016ce652f3a9680586d6302e25617784
Nazar <yaremko.nazar2000@gmail.com>
2021-12-04 12:40:07 +0100

    6

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 
-import flask_whooshalchemy as wa
+#import flask_whooshalchemy as wa
 
 from application.extensions import db
 from application.api import api
@@ -9,24 +9,18 @@ from application.config import Config
 from application.models import Post
 
 
-def create_app(config=Config) -> Flask:
-    app = Flask(__name__)
+app = Flask(__name__)
+app.config.from_object(Config)
+app.app_context().push()
+db.init_app(app)
+app.register_blueprint(api)
+# wa.whoosh_index(app, Post)
+with app.app_context():
+    db.create_all()
+    db.session.commit()
 
-    #app.config.from_object(config)
-    #app.app_context().push()
-    #db.init_app(app)
-    #app.register_blueprint(api)
-    # wa.whoosh_index(app, Post)
-    #with app.app_context():
-        #db.create_all()
-        #db.session.commit()
-    #return app
 
-@api.route("/")
-def index():
-    return {"Server": "is running"}
-    
+
 if __name__ == "__main__":
-    app = create_app()
     app.run()
 
