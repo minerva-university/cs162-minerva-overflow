ad2f225d11adb12bb4bb7b627e01d22616b9e570
Nazar <yaremko.nazar2000@gmail.com>
2021-12-04 12:24:16 +0100

    5

--- app.py
+++ app.py
@@ -1,26 +1,38 @@
 from flask import Flask
 
+
 # import flask_whooshalchemy as wa
 
-from application.extensions import db
-from application.api import api
-from application.config import Config
-from application.models import Post
+#from application.extensions import db
+#from application.api import api
+#from application.config import Config
+#from application.models import Post
 
 
-def create_app(config=Config) -> Flask:
-    app = Flask(__name__)
-    app.config.from_object(config)
-    app.app_context().push()
-    db.init_app(app)
-    app.register_blueprint(api)
+#def create_app(config=Config) -> Flask:
+    #app = Flask(__name__)
+    #app.config.from_object(config)
+    #app.app_context().push()
+    #db.init_app(app)
+    #app.register_blueprint(api)
     # wa.whoosh_index(app, Post)
-    with app.app_context():
-        db.create_all()
-        db.session.commit()
-    return app
+    #with app.app_context():
+        #db.create_all()
+        #db.session.commit()
+    #return app
 
 
-if __name__ == "__main__":
-    app = create_app()
+#if __name__ == "__main__":
+    #app = create_app()
+    #app.run()
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def index():
+    return {
+        "tutorial": "Flask React Heroku"
+    }
+
+if __name__ == '__main__':
     app.run()
