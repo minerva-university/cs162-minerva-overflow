d02245fd2928cf363c8f03492a274d20c59abd73
Nazar <yaremko.nazar2000@gmail.com>
2021-12-09 17:33:52 +0100

    Set up basic React components to test authentification and set up tocken creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,37 @@ import Post from "./components/Post.js";
 import NavBar from "./components/Nav.js";
 import Elem from "./components/RenderElem.js";
 import Tags from "./components/Tags.js";
+import MyForm from "./components/MyForm.js";
+
+import Login from './components/Login/Login';
+import Dashboard from './components/Dashboard/Dashboard';
+import Preferences from './components/Preferences/Preferences';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [token, setToken] = useState();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />}>
+          </Route>
+          <Route path="/preferences" element={<Preferences />}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+/*
     <div className="App">
       <NavBar />
       <header className="App-header">
+      <MyForm />
         <Header contact={{ username: "XYZ" }} />
         <Post name={"First Post"}></Post>
         <Tags />
@@ -18,6 +43,7 @@ function App() {
         <Elem path="/tags" tagName="tag_name"></Elem>
       </header>
     </div>
+*/
   );
 }
 
---
+++ frontend/src/components/MyForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function MyForm() {
+    return (
+      <form>
+        <label>Enter your username:
+          <input type="text" />
+        </label>
+        <br/>
+        <label>Enter your password:
+          <input type="text" />
+        </label>
+      </form>
+    )
+  }
+
+  export default MyForm;
