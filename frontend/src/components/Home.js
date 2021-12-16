1373aaa846b4bcda92e57ed0e10657fbb971add1
Allison Lehn <allison@uni.minerva.edu>
2021-12-16 15:53:36 +0100

    Added minor style changes to Home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -7,10 +7,21 @@ function Home() {
       return <Login setToken={setToken} />
     }
     */
-
+    var mock_data = {
+      name: "Allison Lehn",
+      user_id: 2,
+      email : "allison@uni.minerva.edu"
+    };
   return (
     <div className="Home">
-      <p> This is your home!</p>
+      <header className="App-head">
+        <h2> Welcome to your home, {mock_data.name}!</h2>
+      </header>
+      <p>email : {mock_data.email}</p>
+      <h3>My Posts</h3>
+      {/* get_posts_written_by_user here */}
+      <h3>My Comments</h3>
+      {/* get_comments_written_by_user here */}
     </div>
   );
 }
