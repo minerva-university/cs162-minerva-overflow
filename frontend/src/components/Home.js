7e0c54216c3e74270c504809a10113b271b2eaba
yutaro-shimizu <68834841+yutaro-shimizu@users.noreply.github.com>
2021-12-17 18:36:47 +0900

    show profile details

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./style/Home.css";
+import "./style/Addposts.css";
 import {useAuth, authFetch, login, logout} from "../auth";
 import Login from './Login';
 
 function Home() {
     
+    const [info, setInfo]=useState([])
+    const [show, setShow]=useState(false)
+
     const [logged] = [useAuth()];
       if(!logged[0]) {
         return <Login/>
@@ -15,7 +19,7 @@ function Home() {
 
     const onSubmitClick = (e) => {
       e.preventDefault();
-      //console.log(opts)
+      // console.log(opts)
      fetch("/api/protected", {
         method: "get",
         headers: {
@@ -23,14 +27,38 @@ function Home() {
         }
       })
        .then(r => r.json())
+       .then((res) => setInfo(res))
         .then(rJson => console.log(rJson))
+        .then(setShow(true));
     };
-  
-    
+
+    // fetch("/api/protected", {
+    //   method: "get",
+    //   headers: {
+    //     "Authorization": "Bearer " + my_jwt
+    //   }
+    // })
+    //  .then(r => r.json())
+    //  .then((res) => setInfo(res))
+    //   .then(rJson => console.log(rJson))
+
     return (
       <div className="Home">
-        <p> This is your home!</p>
-        <button onClick={onSubmitClick}> check </button>
+        <h1> Welcome back to Minerva Overflow!</h1>
+        <button className="post_button" onClick={onSubmitClick}> Show user detail </button>
+        <button className="post_button" onClick={()=>setShow(false)}> Hide </button>
+        {
+          show?
+          <div>
+            <p>First name: {info.first_name}</p>
+            <p>Surname: {info.surname}</p>
+            <p>Username: {info.username}</p>
+            <p>Email: {info.email}</p>
+            <p>About Me: {info.about_me}</p>
+            <p>Cohort: {info.cohort_id}</p>
+            {console.log(info)}
+          </div>:null
+        }
       </div>
     );
   }
